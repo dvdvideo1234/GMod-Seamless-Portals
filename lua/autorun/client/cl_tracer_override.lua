c27a2a4f4a58127151812f505528a34b06386740
Deyan Dobromirov <dvd_video@abv.bg>
2026-01-12 12:38:13 +0200

    Tool tracer override initial version

---
+++ lua/autorun/client/cl_tracer_override.lua
@@ -0,0 +1,96 @@
+SeamlessPortals = SeamlessPortals or {}
+
+if CLIENT then timer.Simple(0, function()
+  local all = effects.GetList() or {}
+  local eff = all["ToolTracer"]
+
+  if eff then
+    -- keep the old Render if you want to call it
+    SeamlessPortals.ToolTraceRender = eff.Render
+
+    eff.Render = function(self)
+      if ( self.Alpha < 1 ) then return end
+
+      render.SetMaterial( self.Mat )
+      local texcoord = math.Rand( 0, 1 )
+
+      local norm = ( self.StartPos - self.EndPos ) * self.Life
+
+      self.Length = norm:Length()
+
+      for i = 1, 3 do
+
+        render.DrawBeam( self.StartPos - norm,    -- Start
+              self.EndPos,          -- End
+              8,                -- Width
+              texcoord,           -- Start tex coordinate
+              texcoord + self.Length / 128, -- End tex coordinate
+              color_white )         -- Color (optional)
+      end
+
+      render.DrawBeam( self.StartPos,
+              self.EndPos,
+              8,
+              texcoord,
+              texcoord + ( ( self.StartPos - self.EndPos ):Length() / 128 ),
+              Color( 255, 255, 255, 128 * ( 1 - self.Life ) ) )
+      -- Optionally call SeamlessPortals.ToolTraceRender(self) here if needed
+    end
+
+  else
+    -- Fallback: register a full effect if ToolTracer does not exist yet
+    local EFFECT = {}
+          EFFECT.Mat = Material( "effects/tool_tracer" )
+
+    -- Copy or Implement GetTracerShootPos/Init/Think here
+    -- See base effect in gamemodes/base/entities/effects/tooltracer.lua
+    function EFFECT:Init(data)
+      self.Position   = data:GetStart()
+      self.WeaponEnt  = data:GetEntity()
+      self.Attachment = data:GetAttachment()
+      self.StartPos   = self:GetTracerShootPos(self.Position, self.WeaponEnt, self.Attachment)
+      self.EndPos     = data:GetOrigin()
+      self.Alpha      = 255
+      self.Life       = 0
+      self:SetRenderBoundsWS(self.StartPos, self.EndPos)
+    end
+
+    function EFFECT:Think()
+      self.Life = self.Life + FrameTime() * 4
+      self.Alpha = 255 * (1 - self.Life)
+      return self.Life < 1
+    end
+
+    function EFFECT:Render()
+      if ( self.Alpha < 1 ) then return end
+
+      render.SetMaterial( self.Mat )
+      local texcoord = math.Rand( 0, 1 )
+
+      local norm = ( self.StartPos - self.EndPos ) * self.Life
+
+      self.Length = norm:Length()
+
+      for i = 1, 3 do
+
+        render.DrawBeam( self.StartPos - norm,    -- Start
+              self.EndPos,          -- End
+              8,                -- Width
+              texcoord,           -- Start tex coordinate
+              texcoord + self.Length / 128, -- End tex coordinate
+              color_white )         -- Color (optional)
+      end
+
+      render.DrawBeam( self.StartPos,
+              self.EndPos,
+              8,
+              texcoord,
+              texcoord + ( ( self.StartPos - self.EndPos ):Length() / 128 ),
+              Color( 255, 255, 255, 128 * ( 1 - self.Life ) ) )
+    end
+
+    effects.Register(EFFECT, "ToolTracer")
+  end
+
+
+end) end
